a9464ef19ec1c21e328d8034fdbd24f1ebc5cf7e
kohliaryan <kohliaryan2004@gmail.com>
2024-09-05 23:47:55 +0530

    Adding Sign Up route

--- backend/src/routes/user.ts
+++ backend/src/routes/user.ts
@@ -1,8 +1,28 @@
 import { Hono } from "hono";
+import { PrismaClient } from "@prisma/client/edge";
+import { withAccelerate } from "@prisma/extension-accelerate";
 
-export const userRouter = new Hono();
+export const userRouter = new Hono<{
+	Bindings: {
+		DATABASE_URL: string
+	}
+}>();
 
-userRouter.get("/signup", (c) => {
+userRouter.post("/signup",async (c) => {
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate());
+
+  const body = await c.req.json()
+
+  await prisma.user.create({
+    data: {
+      email: body.email,
+      name: body.name,
+      password: body.password,
+    }
+  })
+
   return c.text("Sign Up");
 });
 
